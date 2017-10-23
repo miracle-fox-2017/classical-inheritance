59bdd8c61abb0e8161e004eee9124299410d1162
Ahmad Fajar & Fickry <pair+ahmad-fajar.biliman@devbootcamp.com>
2017-10-23 16:30:31 +0700

    release 1

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,34 @@
 "use strict"
+
+class Animal{
+  constructor(num_legs, blodded){
+    this.num_leg = num_legs || 2
+    this.is_warm_blodded = blodded || false
+  }
+}
+
+class SuperPower{
+  use_laser_vision(){
+    return 'laser...bipbipbip..'
+  }
+}
+
+
+class Chimpanze extends Animal{
+  constructor(leg, blodd){
+    super(leg, blodd)
+    this.superpower = new SuperPower()
+  }
+}
+
+class Frog extends Animal{
+  constructor(leg, blodd){
+    super(leg, blodd)
+    this.superpower = new SuperPower()
+  }
+}
+
+let chimp = new Chimpanze(8, true)
+let frog = new Frog(12)
+console.log(frog);
+console.log(frog.superpower.use_laser_vision());

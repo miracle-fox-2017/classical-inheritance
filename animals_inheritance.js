59350b4af1d277922f648057e13a260172ae9e07
Ayunda <ayundahamzah@gmail.com>
2017-10-23 19:23:11 +0700

    release 0

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,47 @@
 "use strict"
+class Animal{
+  constructor(){
+    this.jumlahKaki=4;
+    this.Howtheymove='terbang';
+    this.isVegan=true;
+  }
+}
+class Bird extends Animal{
+  constructor(){
+    super()
+    this.jumlahKaki=2;
+
+  }
+}
+class Bat extends Animal{
+  constructor(){
+    super()
+    this.jumlahKaki=2;
+
+  }
+}
+class Fox extends Animal{
+  constructor(){
+    super()
+    this.Howtheymove='Jalan dan Berlari'
+    this.isVegan = false;
+
+  }
+}
+class Wolf extends Animal{
+  constructor(){
+    super()
+    this.Howtheymove='Jalan dan Berlari'
+    this.isVegan = false;
+
+  }
+}
+
+let bird = new Bird()
+console.log(bird);
+let bat = new Bat()
+console.log(bat);
+let fox = new Fox()
+console.log(fox);
+let wolf = new Wolf()
+console.log(wolf);

beef0545ed49ab5a1a3c10f2c2ea406dae67ba21
trijadoel <triamri@gmail.com>
2017-10-23 20:24:52 +0700

    release I

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,28 @@
 "use strict"
+class Animal{
+    constructor(){
+        this.num_legs = 2;
+        this.blood = true;
+    }
+}
+
+class Ayam extends Animal{
+    
+}
+
+class Bebek extends Animal{
+    
+}
+
+class Kucing extends Animal{
+    constructor(kaki) {
+        super();
+        this.num_legs = 4;
+    }
+}
+
+let ayam = new Ayam()
+console.log(ayam);
+
+let kucing = new Kucing(4)
+console.log(kucing);
